4448c6019876e3c232e7d917991fbbfca43c45ad
Wenting Ye <Wenting_Ye@Outlook.com>
2016-10-30 10:23:46 +0800

    finish homework 1
    ~

---
+++ code/YeWenting/t2.asm
@@ -0,0 +1,22 @@
+TITLE T2_YWT
+
+DATASG SEGMENT    
+    FLD1B DB 'personal computer'
+    FLD2B DB 32
+    FLD3B DB 20H
+    FLD4B DB 01011001B
+    FLD5B DB '32654'
+    FLD6B DB 10 DUP(0)
+    FLD7B DB 'PART1', 20, 'PART2', 50, 'PART3', 14
+    
+    FLD1W DW 0FFF0H
+    FLD2W DW 01011001B
+    FLD3W DW FLD7B
+    FLD4W DW 5, 6, 7, 8, 9
+    FLD5W DW 5 DUP(0)
+    FLD6W DW FLD1W - FLD1B     
+ENDS      
+
+
+
+    
